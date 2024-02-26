0d3a60a7f2755e695106b864f646fa4ed587185e
davykoch <koechdavis24@gmail.com>
2024-02-27 01:30:25 +0300

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,19 @@
 # 100-puppet_ssh_config.pp
 
 file { '/etc/ssh/ssh_config':
-  ensure  => present,
+  ensure => present,
 }
 
 augeas { 'Turn off passwd auth':
   context => '/files/etc/ssh/ssh_config',
-  changes => 'set PasswordAuthentication no',
+  changes => [
+    'set Host/*/#comment[.="*"]/PasswordAuthentication no',
+  ],
 }
 
 augeas { 'Declare identity file':
   context => '/files/etc/ssh/ssh_config',
-  changes => 'set IdentityFile ~/.ssh/school',
+  changes => [
+    'set Host/*/#comment[.="*"]/IdentityFile ~/.ssh/school',
+  ],
 }
