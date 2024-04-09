e1bc4b3fc70f5e7c4f0e2e783238013a517d90f9
davykoch <koechdavis24@gmail.com>
2024-04-10 01:56:02 +0300

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,32 +6,33 @@ If an invalid subreddit is given, the function returns 0.
 """
 
 import requests
-import sys
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-                "User-Agent": (
-                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                 "AppleWebKit/537.36 (KHTML, like Gecko) "
-                 "Chrome/58.0.3029.110 Safari/537.3"
-                             )
-              }
+    '''
+    Returns the number of subscribers for a given subreddit.
+    '''
 
-    response = requests.get(url, headers=headers)
+    user_agent = {'User-Agent': 'Lizzie'}
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(url, headers=user_agent)
 
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
+        try:
+            data = response.json()
+            subscribers = data.get('data').get('subscribers')
+            return subscribers
+        except Exception:
+            return 0
     else:
         return 0
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        subreddit = sys.argv[1]
+        subreddit = argv[1]
         print(number_of_subscribers(subreddit))
