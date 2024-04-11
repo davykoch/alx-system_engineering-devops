4867f0e8ba941b71699cbeb82f293bf150caca5b
davykoch <koechdavis24@gmail.com>
2024-04-11 21:13:26 +0300

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# Puppet manifest to fix Apache 500 error by setting correct permissions on the document root directory
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
