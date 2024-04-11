26bde46b399576733624b1cbcfdec92cc85f7c0f
davykoch <koechdavis24@gmail.com>
2024-04-11 21:39:47 +0300

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,21 +9,13 @@ from sys import argv
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
     headers = {'User-Agent': 'YourCustomUserAgent/1.0'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=headers).json()
     try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        data = response.json()
-        return data.get('data', {}).get('subscribers', 0)
-    except requests.exceptions.HTTPError:
-        return 0
+        return url.get('data').get('subscribers')
     except Exception:
         return 0
 
 
 if __name__ == "__main__":
-    if len(argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subscribers = number_of_subscribers(argv[1])
-        print(subscribers)
+    number_of_subscribers(argv[1])
